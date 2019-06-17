23721d99b1acdbb2c638d7ba88f32e3305ca7b8e
Pavlos Makridis <pavlosmakridis1322001@gmail.com>
2019-06-17 13:58:32 +0300

    Add files via upload
    Simplest way to detect motion. Subtracts the last two frames and then calculates the mean of the difference. If the mean is above a defined threshold it detects the motion.

---
+++ motion_detection_by_frame_subtraction.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+text = ""
+
+
+color = (0,0,255) #BGR
+
+cap = cv2.VideoCapture(0) #0 means first webcam
+
+frames = []
+counter = 0
+
+threshold = 1.5
+
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #converts captured frame to Grayscale for easier analysis
+    
+    frames.append(gray)
+    cv2.putText(frame,text,(5,30), font, 1, color, 3, cv2.LINE_AA) #may need to change some arguments 
+
+    cv2.imshow('frame',frame)
+    #cv2.imshow('gray',gray)
+    
+    if counter > 0:
+        difference = cv2.subtract(cv2.medianBlur(frames[counter],15),cv2.medianBlur(frames[counter-1],15))
+        mean = np.mean(difference)
+        print(mean)
+        if mean > threshold:
+            text = "Motion Detected"
+        else:
+            text = ""
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'): #Press q to quit video capture
+        break
+    
+    counter = counter + 1
+    
+
+cap.release()
+cv2.destroyAllWindows()
+
+
